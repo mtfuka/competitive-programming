a6725380817d14a4266d2624dcf90059ecc78cc7
mtfuka <79830375+mtfuka@users.noreply.github.com>
2021-03-03 12:16:50 +0900

    Create A.cpp

---
+++ AtCoder/AtCoder Beginner Contest/064/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+  int r,g,b;
+  cin >> r >> g >> b;
+  int x = 10*g + b;
+
+  if(x%4 == 0){
+    cout << "YES" << endl;
+  }
+  else{
+    cout << "NO" << endl;
+  }
+
+}
