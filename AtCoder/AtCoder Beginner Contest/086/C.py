1638f510383964d253bb2e3ba03748d52aa45123
mtfuka <79830375+mtfuka@users.noreply.github.com>
2021-03-18 16:03:07 +0900

    delete

--- AtCoder/AtCoder Beginner Contest/086/C.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-prev_t = 0
-for i in range(n):
-    t, x, y=map(int, input().split())
-    if (x + y) > (t - prev_t) or (x + y + (t - prev_t)) % 2:
-        print("No")
-        exit()
-    prev_t = t
-print("Yes")
