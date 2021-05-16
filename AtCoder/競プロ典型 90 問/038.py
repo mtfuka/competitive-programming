d402b0a56525c1a0d120939aa4ad1adb6849c37c
mtfuka <mtfuka@gmail.com>
2021-05-16 12:34:54 +0900

    Add 038

--- AtCoder/競プロ典型 90 問/038.py
+++ AtCoder/競プロ典型 90 問/038.py
@@ -1,7 +1,6 @@
 import math
 A,B = map(int,input().split())
 GCD = math.gcd(A,B)
-a = A//GCD
-b = B//GCD
-if a*b>10**18/GCD:print('Large')
-else:print(GCD*a*b)
+C = A//GCD
+if B>10**18//C:print('Large')
+else:print(B*C)
