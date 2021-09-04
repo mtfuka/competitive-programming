dacf5df68ac6b1bd1a726ce9380c30f89d4a1f1e
fukasawa.motoki <fukasawa.motoki@wings-consulting.jp>
2021-09-04 23:07:37 +0900

    Add ABC217

---
+++ AtCoder/AtCoder Beginner Contest/217/B.py
@@ -0,0 +1,11 @@
+S1=input()
+S2=input()
+S3=input()
+if S1!='ABC' and S2!='ABC' and S3!='ABC':
+  print('ABC')
+if S1!='ARC' and S2!='ARC' and S3!='ARC':
+  print('ARC')
+if S1!='AGC' and S2!='AGC' and S3!='AGC':
+  print('AGC')
+if S1!='AHC' and S2!='AHC' and S3!='AHC':
+  print('AHC')
